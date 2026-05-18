1f1e2df2fa4ff183d11f006fd284cfa106382173
Coding Labs <baileyharbach14@outlook.com>
2026-05-18 17:44:38 +0100

    Enhance Simple Times Table Game functionality
    Refactor times table game to track correct and incorrect answers, and add time tracking.

--- Basics /Simple Times Table Game.py
+++ Basics /Simple Times Table Game.py
@@ -1,25 +1,32 @@
-from random import randint
-questions = 10
+from random import randint as r
+from time import time as t
 correct = 0
-wrong = 0
-
+incorrect = 0
+questions = int(input("How Many Qestions Would You Like: "))
+beforeQuestions = questions
+start = t()
 while True:
-    if questions == 0:
-        product = correct / 10 * 100
-        print(f"You got {correct} / 10")
-        print(f"Your percentage was {product}%")
-        break
-    num1 = randint(2,12)
-    num2 = randint(2,12)
-    product = (num1 * num2)
-    answer = int(input(f"{num1} x {num2} = "))
-    
-    if product == answer:
-        print(f"Correct | {questions - 1}")
-        correct += 1
+    if questions == 0 :
+       end = t()
+       print(
+           f"Time Taken {end - start:.2f}\n"
+           f"{correct} Correct\n"
+           f"{incorrect} Incorrect\n"
+           f"{correct} / {beforeQuestions}"
+       )
+       break
+    try:
+        num1, num2 = r(2,12) , r(2,12)
+        product = num1 * num2
+        user = int(input(f"{num1} x {num2} = "))
         questions -= 1
 
-    else:
-        print(f"Incorrect | {questions - 1}")
-        wrong += 1
-        questions -= 1
+        if user == product:
+            correct += 1
+            print(f"Correct | Qestions Left {questions}")
+        else:
+            incorrect += 1
+            print(f"Incorrect | Questions Left {questions}")
+    except ValueError:
+        questions += 1
+        print("Numbers Only")
